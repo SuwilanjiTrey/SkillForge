839ec2050bdcaf733a62fbc5c3af1e62696d8a72
Suwilanji Trey Chellah <143183924+SuwilanjiTrey@users.noreply.github.com>
2025-01-03 14:31:17 +0200

    Update firebase.js

--- src/AnA/firebase.js
+++ src/AnA/firebase.js
@@ -9,13 +9,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAujl-2ANoOK3-_-NObuuvTCBZgNo3Tqpw",
-  authDomain: "skillforge-da0c8.firebaseapp.com",
-  projectId: "skillforge-da0c8",
-  storageBucket: "skillforge-da0c8.firebasestorage.app",
-  messagingSenderId: "287116502790",
-  appId: "1:287116502790:web:ee6df539aa66b51f226087",
-  measurementId: "G-S0R3RPBZKR"
+//config files here
 };
 
 // Initialize Firebase
