6337949c1f6953c1d6baa87eb8e10e65e98592a5
Suwilanji chellah <Suwilanjichellah0228@gmail.com>
2025-04-13 19:30:49 +0200

    enahnced user auth and access

--- src/App.js
+++ src/App.js
@@ -1,33 +1,48 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from "./Home";
 import AdminDashboard from "./Admin/Admin";
 import AdminRoute from "./Admin/AdminRoute";
 import Login from "./AnA/Login";
 import Register from "./AnA/Register";
 import AuthRoute from "./AnA/AuthRoute";
+import ViewerDashboard from "./Viewers/MainInter.jsx";
+import MemberDashboard from "./Members/MainInter.jsx";
 
 const App = () => {
   return (
-    <BrowserRouter basename="/">
-      <Routes>
-        <Route path="/" element={
-          <AuthRoute>
-            <HomePage />
-          </AuthRoute>
-        } />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route
-          path="/admin"
-          element={
-            <AdminRoute>
-              <AdminDashboard />
-            </AdminRoute>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <BrowserRouter>
+  <Routes>
+    {/* Public routes */}
+    <Route path="/" element={<HomePage />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/register" element={<Register />} />
+    <Route path="/viewer" element={<ViewerDashboard />} />
+
+    {/* Protected member routes */}
+    <Route 
+      path="/dashboard" 
+      element={
+        <AuthRoute>
+          <MemberDashboard />
+        </AuthRoute>
+      } 
+    />
+
+    {/* Protected admin routes */}
+    <Route 
+      path="/admin" 
+      element={
+        <AdminRoute>
+          <AdminDashboard />
+        </AdminRoute>
+      } 
+    />
+
+    {/* Catch all route */}
+    <Route path="*" element={<Navigate to="/" />} />
+  </Routes>
+</BrowserRouter>
   );
 };
 
